bc88c29240caa3cf7bbc774ca1c585c3936ea331
jainam shah <jainamshah006@gmail.com>
2021-04-14 02:29:22 +0530

    validation changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,27 +1,6 @@
-// var mysql = require('mysql');
-
-// var connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'rope_master',
-// });
-
-// connection.connect((err) => {
-//   if (!err) {
-//     console.log("Database is connected ...");
-//   } else {
-//     console.log("" + err + " : Error connecting database ...");
-//   }
-// });
-
-// module.exports = connection;
-
 const mysql = require('mysql');
-// mysql.createConnection({multipleStatements: true});
 
 const db = mysql.createPool({
-  // connectionLimit : 100,
   connectionLimit : 1000,
   connectTimeout  : 60 * 60 * 1000,
   acquireTimeout  : 60 * 60 * 1000,
@@ -29,9 +8,10 @@ const db = mysql.createPool({
   host: 'neersytems.ckuhkerjn7n2.us-east-2.rds.amazonaws.com',
   user: 'admin',
   password: 'Ja!nam97',
+  // host: 'localhost',
+  // user: 'root',
+  // password: '',
   port: 3306,
-  // connectTimeout: 20000,
-  // multipleStatements: true,
   database: 'neer_system',
 });
 
