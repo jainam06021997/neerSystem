42c64be64a58ef63214edb6bdd854d0e0c51da28
dsarjit <>
2021-08-28 23:19:15 +0530

    db disconnected

--- api/contactUsAPI.js
+++ api/contactUsAPI.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var contactUsCtrl = require('./../ctrl/contactUsCtrl');
+// var contactUsCtrl = require('./../ctrl/contactUsCtrl');
 
 router.get('/', async (req, res) => {
   try{
--- ctrl/contactUsCtrl.js
+++ ctrl/contactUsCtrl.js
@@ -1,43 +1,43 @@
-var express = require('express');
-var router = express.Router();
-var {db, connectionRelease} = require('../config/dbConfig');
+// var express = require('express');
+// var router = express.Router();
+// var {db, connectionRelease} = require('../config/dbConfig');
 
-router.list = (req) => {
-  return new Promise((resolve, reject) => {
-    const query = `select * from contactus order by created_at desc`;
-    db.query(query, (err, result) => {
-      connectionRelease;
-      if (err) {
-        const msg = {
-          message: 'db error',
-          error: err,
-        };
-        reject(msg);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-};
+// router.list = (req) => {
+//   return new Promise((resolve, reject) => {
+//     const query = `select * from contactus order by created_at desc`;
+//     db.query(query, (err, result) => {
+//       connectionRelease;
+//       if (err) {
+//         const msg = {
+//           message: 'db error',
+//           error: err,
+//         };
+//         reject(msg);
+//       } else {
+//         resolve(result);
+//       }
+//     });
+//   });
+// };
 
-router.contactus = (data) => {
-  return new Promise((resolve, reject) => {
-    db.query('insert into contactus set ? ', data, (err, res) => {
-      connectionRelease;
-      if (err) {
-        const msg = {
-          message: 'db error',
-          error: err,
-        };
-        reject(msg);
-      } else {
-        const msg = {
-          message: 'Record Added Successfully',
-        };
-        resolve(msg);
-      }
-    });
-  });
-};
+// router.contactus = (data) => {
+//   return new Promise((resolve, reject) => {
+//     db.query('insert into contactus set ? ', data, (err, res) => {
+//       connectionRelease;
+//       if (err) {
+//         const msg = {
+//           message: 'db error',
+//           error: err,
+//         };
+//         reject(msg);
+//       } else {
+//         const msg = {
+//           message: 'Record Added Successfully',
+//         };
+//         resolve(msg);
+//       }
+//     });
+//   });
+// };
 
-module.exports = router;
+// module.exports = router;
